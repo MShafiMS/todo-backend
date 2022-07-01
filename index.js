c455217b1e013ad7236cecf2c2735dd86ed9dd3c
MShafiMS <shahedali734@gmail.com>
2022-07-01 12:41:05 -0600

    second

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+}
+
+
+app.use(cors(corsConfig))
+app.options("*", cors(corsConfig))
+app.use(function (req, res, next) {
+res.header("Access-Control-Allow-Origin", "*")
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,authorization")
+next()
+})
 app.use(express.json());
 
 
@@ -28,6 +42,13 @@ async function run(){
           const todos = await todoCollection.find(query).toArray();
           res.send(todos);
         });
+
+        app.get('/todoList/:id', async(req, res) =>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const result = await todoCollection.findOne(query);
+          res.send(result);
+      });
 
         app.delete("/todoList/:id", async (req, res) => {
           const id = req.params.id;
